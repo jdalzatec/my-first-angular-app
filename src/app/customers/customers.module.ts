e9945c33baefa36344c9b95959afb810196e16c2
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2021-04-13 17:40:54 -0500

    Implement customer list component

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomersComponent } from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
-  declarations: [CustomersComponent],
+  declarations: [
+    CustomersComponent,
+    CustomersListComponent,
+    FilterTextboxComponent,
+  ],
   imports: [CommonModule],
   exports: [CustomersComponent],
 })
