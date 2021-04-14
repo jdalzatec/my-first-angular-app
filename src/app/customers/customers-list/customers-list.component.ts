cb826c2205862f1383b669023fc586d5fd45e664
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2021-04-13 19:15:02 -0500

    Implement some interfaces for customer, orders and order item

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ICustomer } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
-  styleUrls: ['./customers-list.component.css']
+  styleUrls: ['./customers-list.component.css'],
 })
 export class CustomersListComponent implements OnInit {
+  filteredCustomers: ICustomer[] = [];
+  customersOrderTotal: number;
+  currencyCode: string = 'USD';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
