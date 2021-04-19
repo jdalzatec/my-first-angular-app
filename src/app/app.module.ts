fd45f899e36fe6f90ad50d5a242ac832afae92ba
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2021-04-18 22:34:56 -0500

    Add route for customers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    CustomersModule,
+    SharedModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
